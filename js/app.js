6729e6247beb57efeb285283f7e0f194f096d114
Giovanne Silva <giovanne.silva@tech4h.com.br>
2023-04-10 19:07:55 -0300

    Projeto star wars javascript basico

---
+++ js/app.js
@@ -0,0 +1,85 @@
+
+const LIST = [
+    {
+        id: 1,
+        nome: 'Mestre Yoda',
+        avatar: 'images/yoda.png'
+    },
+    {
+        id: 2,
+        nome: 'Luke Skywalker',
+        avatar: 'images/luke.png'
+    },
+    {
+        id: 3,
+        nome: 'Princesa Leia',
+        avatar: 'images/leia.png'
+    },
+    {
+        id: 4,
+        nome: 'Chewbacca',
+        avatar: 'images/chewbacca.png'
+    },
+    {
+        id: 5,
+        nome: 'Darth Vader',
+        avatar: 'images/vader.png'
+    },
+    {
+        id: 6,
+        nome: 'Han Solo',
+        avatar: 'images/hansolo.png'
+    },
+    {
+        id: 7,
+        nome: 'C3po',
+        avatar: 'images/c3po.png'
+    },
+    {
+        id: 8,
+        nome: 'R2d2',
+        avatar: 'images/r2d2.png'
+    }
+]
+
+
+const App = new Vue({
+    el: '#app',
+    data: {
+        title: 'Star Wars Lego',
+        userName: 'Giovanne',
+        characters: LIST,
+        searchName: ''
+    },
+    methods: {
+        like(userName) {
+            alert(`O personagem ${userName} recebeu um like!`)
+        },
+        remove(id) {
+            const list = this.characters
+
+            const result = list.filter(item => {
+                return item.id !== id
+            })
+
+            this.characters = result
+        },
+        search() {
+
+            if (this.searchName === '') {
+                return alert('Preencha o campo')
+            }
+            const list = this.characters = LIST
+
+            const result = list.filter(item => {
+                return item.nome === this.searchName
+            })
+
+            if (result.length <= 0) {
+                alert('Não encontrado')
+            } else {
+                this.characters = result
+            }
+        }
+    }
+})
---
+++ js/basic.js
@@ -0,0 +1,38 @@
+console.log('Olá, Javacript')
+
+var userName = 'Giovanne Silva'
+
+document.getElementById('user-Name').innerHTML = userName
+
+
+//var personagens = ['Mestre Yoda', 'Luke Skywalker', 'Princesa Leia', 'Darth Vader']
+
+//personagens.forEach(function(p){
+    //console.log(p)
+//})
+
+//for (var i in personagens) {
+    //console.log(personagens[i])
+//}
+
+//for (var i = 0; i <= 10; i++) {
+   // console.log(i)
+//}
+
+/* var yoda = {
+    nome: 'Mestre Yoda',
+    idade: 100,
+    jedi: true,
+    mostraIdade: function() {
+        console.log(`A idade do ${this.nome} é ${this.idade} anos.`)
+    }
+}
+
+console.log(yoda)
+yoda.mostraIdade() */
+
+const nome = 'Princesa Leia'
+    console.log(nome)
+
+nome = 'Yoda'
+console.log(nome)
